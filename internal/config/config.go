8d499a903382cbb180c3e1149920d3281c1c35ee
iwa <iwaongit@gmail.com>
2026-01-06 23:29:58 +0100

    feat: create Config struct

---
+++ internal/config/config.go
@@ -0,0 +1,53 @@
+package config
+
+import (
+	"log"
+	"os"
+)
+
+type Config struct {
+	DiscordToken string
+
+	AIToken string
+	AIMode  AIMode
+	AIModel string
+}
+
+// AIMode enum
+type AIMode string
+
+const (
+	AgentMode AIMode = "agent"
+	ChatMode  AIMode = "chat"
+)
+
+func NewConfig(discordToken, aiToken, aiMode, aiModel string) *Config {
+	return &Config{
+		DiscordToken: discordToken,
+		AIToken:      aiToken,
+		AIMode:       AIMode(aiMode),
+		AIModel:      aiModel,
+	}
+}
+
+func GenerateConfigFromEnv() *Config {
+	getRequiredEnv := func(key string) string {
+		if value := os.Getenv(key); value != "" {
+			return value
+		}
+		log.Fatalf("%s environment variable is required", key)
+		return ""
+	}
+
+	discordToken := getRequiredEnv("DISCORD_TOKEN")
+	aiToken := getRequiredEnv("AI_TOKEN")
+	aiModel := getRequiredEnv("AI_MODEL")
+
+	aiMode := os.Getenv("AI_MODE")
+	if aiMode == "" || (aiMode != string(AgentMode) && aiMode != string(ChatMode)) {
+		aiMode = "chat"
+		log.Printf("AI_MODE not set or invalid, defaulting to %s", aiMode)
+	}
+
+	return NewConfig(discordToken, aiToken, aiMode, aiModel)
+}
